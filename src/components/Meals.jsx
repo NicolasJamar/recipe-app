2b0b3555478603b42e35606f47a42d1ff42cf293
NicolasJamar <nicolas.jamar@gmail.com>
2022-11-24 18:32:31 +0100

    No meals found

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,7 +2,23 @@ import { useGlobalContext } from "../context";
 import { BsHandThumbsUp } from "react-icons/bs";
 
 const Meals = () => {
-  const { meals } = useGlobalContext();  
+  const { loading, meals } = useGlobalContext();  
+  if(loading) {
+    return (
+      <section className="section">
+        <h4>Loading...</h4>
+      </section>
+    )
+  }
+
+  if(meals.length < 1) {
+    return(
+      <section className="section">
+        <h4>No meals</h4>
+      </section>
+    )
+  }
+
   return(
     <section className="section-center">
       { meals.map( (singleMeal) => {
--- src/context.jsx
+++ src/context.jsx
@@ -7,15 +7,22 @@ const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
 
 const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false)
   const [meals, setMeals] = useState([])
 
   const fetchMeals = async(url) => {
+    setLoading(true)
     try {
       const { data } = await axios.get(url)
-      setMeals(data.meals)
+      if(data.meals) {
+        setMeals(data.meals)
+      } else {
+        setMeals([])
+      }
     } catch (error) {
       console.log(error.response);
     }
+    setLoading(false)
   }
   
 
@@ -23,7 +30,7 @@ const AppProvider = ({ children }) => {
     fetchMeals(allMealsUrl)
   }, [])
 
-  return <AppContext.Provider value={{ meals }}>
+  return <AppContext.Provider value={{ loading, meals }}>
       {children}
     </AppContext.Provider>
 }
